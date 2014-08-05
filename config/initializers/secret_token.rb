0379a3f071d9981558a2487cd3056d1a3cbae570
firestarter <alex@ubuntu.(none)>
2014-08-05 23:41:51 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Daria::Application.config.secret_token = '34eba31efa47868f6dd71cf32c83f9d5be106dfab19d486fa271a3ffdae1985e7fb0479f14eb376d339cee252eb85392b018e4d8d6c2f8506dd564f6cf98faf4'
